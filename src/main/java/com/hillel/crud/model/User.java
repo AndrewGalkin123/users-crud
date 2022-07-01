cb5675cd10c5afbaa8914bda02ef2c22d9a4d081
AndrewGalkin <127788127788a@gmail.com>
2022-07-01 23:39:29 +0300

    .

---
+++ src/main/java/com/hillel/crud/model/User.java
@@ -0,0 +1,61 @@
+package com.hillel.crud.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity(name = "users")
+public class User {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    int id;
+    @NotBlank(message = "Name is mandatory")
+    String name;
+    @NotBlank(message = "Email is mandatory")
+    String email;
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
